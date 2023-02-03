7b180e9ddaf2c012ea9f1a38fdcdce5183f7842f
Yann Assiri <yannassiri26@gmail.com>
2023-02-03 15:24:40 +0000

    modif

--- scraping.py
+++ scraping.py
@@ -1,36 +1,53 @@
+# import requests
+# from bs4 import BeautifulSoup
+# # import urllib.request
+
+
+# url = 'https://www.jumia.ci/index/allcategories/'
+# BASE = "https://www.jumia.ci"
+
+# response = requests.get(url)
+# page = response.content
+# soup = BeautifulSoup(page, 'html.parser')
+# CateNom = []
+
+# div = soup.find_all('div', class_="col4 -pvm -bb")
+# for i  in div:
+#     Categories = i.find_all('Categories')
+#     for Categorie in Categories:
+#         if Categorie.find('a').get('href'):
+#             CateNom = Categorie.find('a').get('href')
+# print(Categories)
+
 import requests
 from bs4 import BeautifulSoup
 import pyodbc
 
-
-# Step 1: Collect data from website
-url = "https://www.jumia.ci/electronique/"
+url = 'https://www.jumia.ci/index/allcategories/'
 BASE = "https://www.jumia.ci"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
 
+response = requests.get(url)
+page = response.content
+soup = BeautifulSoup(page, 'html.parser')
 
-# Extract product information
-Produits = [Produits.text for Produits in soup.find_all("div", class_="name")]
+Categories = []
 
+divs = soup.find_all('div', class_="col4 -pvm -bb")
+for div in divs:
+    links = div.find_all('a')
+    for link in links:
+        Categories.append(link.text)
 
-    # category = product.find("span", class_="category").text
-    # tag = product.find("span", class_="tag").text
-    # price = product.find("span", class_="price").text
-    # products.append((product_name, category, tag, price))
+# print(Categories)
 
-# Step 2: Connect to SQL Server database
 conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                       "Server=ASSIRI-LAPTOP;"
                       "Database=dbScrapingETL;"
                       "Trusted_Connection=yes;")
 cursor = conn.cursor()
 
-
-# Step 3: Insert data into SQL Server database
-# cursor.execute("CREATE TABLE Products (ProductName varchar(255), Category varchar(255), Tag varchar(255), Price float)")
-for NomProd in Produits:
-    cursor.execute("INSERT INTO Produits (NomProd) VALUES (?)", NomProd)
+for CateNom in Categories:
+    cursor.execute("INSERT INTO Categories (CateNom) VALUES (?)", CateNom)
 conn.commit()
 
 # Step 4: Close connection
