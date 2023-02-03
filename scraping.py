af7403866a2a2dbbb986ed51b6070f661737ebd3
Yann Assiri <yannassiri26@gmail.com>
2023-02-03 19:38:44 +0000

    modif

--- scraping.py
+++ scraping.py
@@ -1,27 +1,14 @@
-# import requests
-# from bs4 import BeautifulSoup
-# # import urllib.request
-
-
-# url = 'https://www.jumia.ci/index/allcategories/'
-# BASE = "https://www.jumia.ci"
-
-# response = requests.get(url)
-# page = response.content
-# soup = BeautifulSoup(page, 'html.parser')
-# CateNom = []
-
-# div = soup.find_all('div', class_="col4 -pvm -bb")
-# for i  in div:
-#     Categories = i.find_all('Categories')
-#     for Categorie in Categories:
-#         if Categorie.find('a').get('href'):
-#             CateNom = Categorie.find('a').get('href')
-# print(Categories)
-
 import requests
 from bs4 import BeautifulSoup
 import pyodbc
+
+
+conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                      "Server=ASSIRI-LAPTOP;"
+                      "Database=dbScrapingETL;"
+                      "Trusted_Connection=yes;")
+cursor = conn.cursor()
+
 
 url = 'https://www.jumia.ci/index/allcategories/'
 BASE = "https://www.jumia.ci"
@@ -31,24 +18,31 @@ page = response.content
 soup = BeautifulSoup(page, 'html.parser')
 
 Categories = []
+Tags = []
+Produits = []
+
+categories = soup.find_all("a", class_="-pbm -m -upp -hov-or5")
+for category in categories:
+    Categories.append(category.text)
 
 divs = soup.find_all('div', class_="col4 -pvm -bb")
 for div in divs:
     links = div.find_all('a')
     for link in links:
-        Categories.append(link.text)
+        Tags.append(link.text)
 
-# print(Categories)
+produits = soup.find_all('div', class_ ="-pvs col12")
+for produit in produits:
+    if produit.find('h3', class_="name"):
+        noms = url + produit.find_all('h3')
+        Produits.append(noms.text)
+print(Produits)
 
-conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
-                      "Server=ASSIRI-LAPTOP;"
-                      "Database=dbScrapingETL;"
-                      "Trusted_Connection=yes;")
-cursor = conn.cursor()
-
-for CateNom in Categories:
-    cursor.execute("INSERT INTO Categories (CateNom) VALUES (?)", CateNom)
-conn.commit()
+# for TagNom in Tags:
+#     cursor.execute("INSERT INTO Tags (TagNom) VALUES (?)", TagNom)
+# for CateNom in Categories:
+#     cursor.execute("INSERT INTO Categories (CateNom) VALUES (?)", CateNom)
+# conn.commit()
 
 # Step 4: Close connection
 conn.close()
