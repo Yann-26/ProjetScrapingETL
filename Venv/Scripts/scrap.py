fd13cc9f7f64d4fcc22cbd261b3ff80727365839
Yann Assiri <yannassiri26@gmail.com>
2023-02-03 20:46:26 +0000

    upgrade

--- Venv/Scripts/scrap.py
+++ Venv/Scripts/scrap.py
@@ -1,23 +1,37 @@
-#import BeautifulSoup
+import requests
 from bs4 import BeautifulSoup
 
-#import request
-import requests
+product_dict = {}
+BASE_URL = "https://www.jumia.ci"
 
-#retrieve html of target page
-#and prepare for page parsing by BeautifulSoup
-r=requests.get("https://www.jumia.ci/electronique/")
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
+response = requests.get(BASE_URL + "/index/allcategories/")
+page = response.content
+soup = BeautifulSoup(page, 'html.parser')
 
-#return author name
-for h1 in soup.findAll('h1'):
-        print (h1.get)
+categories = soup.find_all("a", class_="-pbm -m -upp -hov-or5")
+for category in categories:
+    category_name = category.text
+    category_url = BASE_URL + category['href']
+    category_response = requests.get(category_url)
+    category_page = category_response.content
+    category_soup = BeautifulSoup(category_page, 'html.parser')
+    category_tags = category_soup.find_all('div', class_="col4 -pvm -bb")
+    for category_tag in category_tags:
+        tag_name = category_tag.find('a').text
+        tag_url = BASE_URL + category_tag.find('a')['href']
+        tag_response = requests.get(tag_url)
+        tag_page = tag_response.content
+        tag_soup = BeautifulSoup(tag_page, 'html.parser')
+        tag_products = tag_soup.find_all('div', class_ ="-pvs col12")
+        for tag_product in tag_products:
+            if tag_product.find('h3', class_="name"):
+                tag_product_name = tag_product.find('h3', class_="name").text
+                if category_name in product_dict:
+                    if tag_name in product_dict[category_name]:
+                        product_dict[category_name][tag_name].append(tag_product_name)
+                    else:
+                        product_dict[category_name][tag_name] = [tag_product_name]
+                else:
+                    product_dict[category_name] = {tag_name: [tag_product_name]}
 
-#returns text for tip title followed by date
-for li in soup.findAll('li'):
-        print (li.text)
-
-#return url for links on a page
-for link in soup.findAll('a'):
-        print (link.get('href'))
+print(product_dict)
--- scraping.py
+++ scraping.py
@@ -10,20 +10,21 @@ conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
 cursor = conn.cursor()
 
 
-url = 'https://www.jumia.ci/index/allcategories/'
-BASE = "https://www.jumia.ci"
-
-response = requests.get(url)
+BASE_URL = "https://www.jumia.ci"
+response = requests.get(BASE_URL + "/index/allcategories/")
 page = response.content
 soup = BeautifulSoup(page, 'html.parser')
 
+
 Categories = []
 Tags = []
-Produits = []
+Produits = {}
+
 
 categories = soup.find_all("a", class_="-pbm -m -upp -hov-or5")
 for category in categories:
     Categories.append(category.text)
+
 
 divs = soup.find_all('div', class_="col4 -pvm -bb")
 for div in divs:
@@ -31,18 +32,43 @@ for div in divs:
     for link in links:
         Tags.append(link.text)
 
-produits = soup.find_all('div', class_ ="-pvs col12")
-for produit in produits:
-    if produit.find('h3', class_="name"):
-        noms = url + produit.find_all('h3')
-        Produits.append(noms.text)
-print(Produits)
+# scrappage des produits 
+categories = soup.find_all("a", class_="-pbm -m -upp -hov-or5")
+for category in categories:
+    category_name = category.text
+    category_url = BASE_URL + category['href']
+    category_response = requests.get(category_url)
+    category_page = category_response.content
+    category_soup = BeautifulSoup(category_page, 'html.parser')
+    category_tags = category_soup.find_all('div', class_="col4 -pvm -bb")
+    for category_tag in category_tags:
+        tag_name = category_tag.find('a').text
+        tag_url = BASE_URL + category_tag.find('a')['href']
+        tag_response = requests.get(tag_url)
+        tag_page = tag_response.content
+        tag_soup = BeautifulSoup(tag_page, 'html.parser')
+        tag_products = tag_soup.find_all('div', class_ ="-pvs col12")
+        for tag_product in tag_products:
+            if tag_product.find('h3', class_="name"):
+                tag_product_name = tag_product.find('h3', class_="name").text
+                if category_name in Produits:
+                    if tag_name in Produits[category_name]:
+                        Produits[category_name][tag_name].append(tag_product_name)
+                    else:
+                        Produits[category_name][tag_name] = [tag_product_name]
+                else:
+                    Produits[category_name] = {tag_name: [tag_product_name]}
 
-# for TagNom in Tags:
-#     cursor.execute("INSERT INTO Tags (TagNom) VALUES (?)", TagNom)
-# for CateNom in Categories:
-#     cursor.execute("INSERT INTO Categories (CateNom) VALUES (?)", CateNom)
-# conn.commit()
+
+
+## insertion des données dans sql server 
+for TagNom in Tags:
+    cursor.execute("INSERT INTO Tags (TagNom) VALUES (?)", TagNom)
+for CateNom in Categories:
+    cursor.execute("INSERT INTO Categories (CateNom) VALUES (?)", CateNom)
+for NomProd in Categories:
+    cursor.execute("INSERT INTO Produitss (NomProd) VALUES (?)", NomProd)
+conn.commit()
 
 # Step 4: Close connection
 conn.close()
